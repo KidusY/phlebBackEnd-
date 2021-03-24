0d3386900da5ee639bb1dd66a01e38760a917200
kidus yilma <kidusyilma@gmail.com>
2021-03-23 20:50:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const db_URL = process.env.DATABASE_URL
+
+const express = require ('express')
+const app = express();
+
+const home = require('./routes/home')
+
+
+
+const mongoose = require ('mongoose');
+mongoose.connect(db_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+
+db.on('error', error=> console.log(error))
+db.on('open', ()=> console.log("Connected to Mongoose"));
+
+app.use('/',home);
+
+app.listen(process.env.PORT || 8000)
