193bdc235b8a6ccefe2556260f126e0a1df77d98
Kidus <kidusyilma@gmail.com>
2021-03-26 21:15:24 -0700

    fixed error message

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,42 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models/users');
 const CommonServices = require('../services/commonServices');
-router.post('/',async(req,res)=>{
-    const {email,password} = req.body
-  
+router.post('/', async (req, res) => {
+    const { email, password } = req.body
+
 
     try {
-        const users = await Users.find({"email":email});
-       
-        if(users.length > 0){
+        const users = await Users.find({ "email": email });
+
+        if (users.length > 0) {
             console.log(password, users.password);
-             CommonServices.comparePasswords(password,users[0].password)
-             .then(compareMatch=>{
-                 
-                 if(!compareMatch){
-                     res.status(400).json({
-                         error:"Incorrect user name or password"
-                     })
-                 }
-                
-            res.status(201).json( {
-             token:CommonServices.createJwt(users[0].email,{user_id:users[0]._id}),
-                email: users[0].email,
-                name: users[0].name
-            
-            })
+            CommonServices.comparePasswords(password, users[0].password)
+                .then(compareMatch => {
 
-             })
-             .catch(err=>console.log(err))
+                    if (!compareMatch) {
+                        res.status(401).json({ errorMessage: "Incorrect User name or password" })
+                    }
+
+                    res.status(201).json({
+                        token: CommonServices.createJwt(users[0].email, { user_id: users[0]._id }),
+                        email: users[0].email,
+                        name: users[0].name
+
+                    })
+
+                })
+                .catch(err => console.log(err))
 
 
 
-            
+
         }
-        else{
-            res.status(401).json({errorMessage:"Incorrect User name or password"})
+        else {
+            res.status(401).json({ errorMessage: "Incorrect User name or password" })
         }
-        
+
     }
     catch (err) {
         console.log(err)
@@ -49,4 +47,4 @@ router.post('/',async(req,res)=>{
 })
 
 
-module.exports= router
+module.exports = router
